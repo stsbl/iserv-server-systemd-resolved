7320824ac3c3015633bc84d0a3e24165aa1cac75
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-05-18 19:30:08 +0200

    Record before bind support removal

--- iconf/etc/resolvconf/update.d/bind/20server-systemd-resolved_cat.sh
+++ iconf/etc/resolvconf/update.d/bind/20server-systemd-resolved_cat.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-if [ ! -f "/usr/share/iserv/iconf/etc/resolvconf/update.d/bind/20portal" ] || [ -f "$FN.distrib" ]
+if [ ! -f "/usr/share/iserv/iconf/etc/resolvconf/update.d/bind/20portal" ] 
 then
-  cat "$FN.distrib"
-else
-  echo "Template missing" >&2
-  exit 1
+  if [ -f "$FN.distrib" ]
+  then
+    cat "$FN.distrib"
+  else
+    echo "Template missing" >&2
+    exit 1
+  fi
 fi
